c2ea0cfde2fdf82602f5740e6b85a5cf8f7d1808
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-04-02 00:15:05 +0800

    baru

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../public/css/style.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
