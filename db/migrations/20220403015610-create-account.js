e6147490a2b79d2b13e22d6d1b7167234e162917
Andi Fajar Nugraha <andifajarlah@gmail.com>
2022-04-04 09:56:25 +0800

    migrateg

---
+++ db/migrations/20220403015610-create-account.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      id_contract: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.TINYINT
+      },
+      id_parent: {
+        type: Sequelize.BIGINT
+      },
+      wallet_address: {
+        type: Sequelize.STRING
+      },
+      email_verification: {
+        type: Sequelize.TINYINT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Accounts');
+  }
+};
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -9,6 +9,8 @@ import Link from 'next/link';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import Web3Modal from 'web3modal';
 import Web3 from 'web3';
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 const providerOptions = {
   walletconnect: {
@@ -96,10 +98,20 @@ function reducer(state: StateType, action: ActionType): StateType {
 const initialValues = {
   username: '',
   email: '',
-  package: '',
+  contract: '',
   password: '',
   acceptTerms: false,
 };
+
+const SignupFormSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  contract: Yup.string().required("Contract is required"),
+  email: Yup.string().email("Email is invalid").required("Email is required"),
+  password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+  acceptTerms: Yup.bool().oneOf([true], "Accept Ts & Cs is required"),
+});
 
 const SignUp: NextPage = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -123,14 +135,15 @@ const SignUp: NextPage = () => {
 
   const connect = useCallback(
     async () => {
-      const provider = await web3Modal.connect()
-  
-      const web3Provider = new Web3(provider)
-  
-      const signer = await web3Provider.eth.getAccounts()
-      const address = await signer[0];
+      try {
+        const provider = await web3Modal.connect();
+      
+        const web3Provider = new Web3(provider);
+    
+        const signer = await web3Provider.eth.getAccounts();
+        const address = await signer[0];
 
-      const network = await web3Provider.eth.net.getId()
+        const network = await web3Provider.eth.net.getId();
         dispatch({
           type: 'SET_WEB3_PROVIDER',
           provider,
@@ -138,27 +151,30 @@ const SignUp: NextPage = () => {
           address,
           chainId: network
         });
+      } catch (error) {
+        console.log(error);
+      }
     }, []
   );
 
   const disconnect = useCallback(
     async function () {
-      await web3Modal.clearCachedProvider()
+      await web3Modal.clearCachedProvider();
+      
       if (provider?.disconnect && typeof provider.disconnect === 'function') {
         await provider.disconnect()
       }
       dispatch({
         type: 'RESET_WEB3_PROVIDER'
       })
-    },
-    [provider]
-  )
+    }, [provider]
+  );
 
   useEffect(() => {
     if (web3Modal.cachedProvider) {
       connect()
     }
-  }, [connect])
+  }, [connect]);
 
   return (
     <div>
@@ -181,50 +197,64 @@ const SignUp: NextPage = () => {
                   console.log(token)
                 } } /> */}
                 {web3Provider ? (
-                  <form>
-                    <div>
-                      <div className='row'>
-                        <div className='col-12 mb-2'>
-                            <label className='form-label'>Username</label>
-                            <input name='username' type='text' className='form-control' required />
-                        </div>
-                        <div className='col-12 mb-2'>
-                            <label className='form-label'>Email</label>
-                            <input name='email' type='email' className='form-control' required />
-                        </div>
-                        <div className='col-12 mb-2'>
-                            <label className='form-label'>Package</label>
-                            <select name='contract' defaultValue={''} className='form-control' required >
-                                <option value='' hidden>-- Select Package --</option>
-                                {contracts.map((contract) => (
-                                  <option value={contract.id} key={contract.id}>{contract.name} - $ {contract.value}</option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className='col-12 mb-2'>
-                            <label className='form-label'>Password</label>
-                            <input name='password' type='password' className='form-control' required />
-                        </div>
-                        <div className='col-12'>
-                          <div className='form-check'>
-                            <input type='checkbox' name='acceptTerms' className='form-check-input' required />
-                            <label className='form-check-label'>
-                              I certify that I am 18 years of age or older, and agree to the <a href='#' className='text-primary'>User Agreement</a> and <a href='#' className='text-primary'>Privacy Policy</a>.
-                            </label>
+                  <>
+                    <Formik initialValues={initialValues} validationSchema={SignupFormSchema} onSubmit={async (fields) => {
+                      alert(JSON.stringify(fields, null, 4));
+                    } }>
+                      {({ errors, status, touched }) => (
+                        <Form>
+                          <div>
+                            <div className='row'>
+                              <div className='col-12 mb-2'>
+                                <label className='form-label'>Username</label>
+                                <input name='username' type='text' autoComplete='off' className={ "form-control" + (errors.username && touched.username? " is-invalid": "") } />
+                                <ErrorMessage name="username" component="div" className="invalid-feedback" />
+                              </div>
+                              <div className='col-12 mb-2'>
+                                <label className='form-label'>Email</label>
+                                <input name='email' type='email' autoComplete='off' className={ "form-control" + (errors.email && touched.email? " is-invalid": "") } />
+                                <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                              </div>
+                              <div className='col-12 mb-2'>
+                                <label className='form-label'>Package</label>
+                                <select name='contract' defaultValue={''} className={ "form-control" + (errors.contract && touched.contract? " is-invalid": "") }>
+                                  <option value='' hidden>-- Select Package --</option>
+                                  {contracts.map((contract) => (
+                                    <option value={contract['id']} key={contract['id']}>{contract['name']} - $ {contract['value']}</option>
+                                  ))}
+                                </select>
+                                <ErrorMessage name="contract" component="div" className="invalid-feedback" />
+                              </div>
+                              <div className='col-12 mb-2'>
+                                <label className='form-label'>Password</label>
+                                <input name='password' autoComplete='off' type='password' className={ "form-control" + (errors.password && touched.password? " is-invalid": "") } />
+                                <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                              </div>
+                              <div className='col-12'>
+                                <div className='form-check'>
+                                  <input type='checkbox' name='acceptTerms' className='form-check-input'  />
+                                  <label className='form-check-label'>
+                                    I certify that I am 18 years of age or older, and agree to the <a href='#' className='text-primary'>User Agreement</a> and <a href='#' className='text-primary'>Privacy Policy</a>.
+                                  </label>
+                                </div>
+                              </div>
+                            </div>
+                            <div className='mt-3 d-grid gap-2'>
+                              <button type='submit' className='btn btn-primary mr-2'>
+                                Sign Up
+                              </button>
+                            </div>
+                            <div className='mt-3 d-grid gap-2'>
+                              <button typeof='button' className='button btn btn-danger' type='button' onClick={disconnect}>
+                                Disconnect From {address?.substring(0, 5)} ... {address?.substring(address.length - 5, address.length)}
+                              </button>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                      <div className='mt-3 d-grid gap-2'>
-                        <button type='submit' className='btn btn-primary mr-2'>
-                          Sign Up
-                        </button>
-                        <hr />
-                        <button typeof='button' className='button btn btn-danger' type='button' onClick={disconnect}>
-                          Disconnect From {address?.substring(0,5)} ... {address?.substring(address.length-5, address.length)}
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                        </Form>
+                      )}
+
+                    </Formik>
+                  </>
                 ) : (
                   <div className='text-center'>
                     <button className='button btn btn-success' type='button' onClick={connect}>
